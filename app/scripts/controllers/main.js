fc87245e6e22fd8cc9049b24bc09b42e17e0e5cc
Nicolas Panel <nicolas.panel@live.fr>
2014-02-10 05:10:40 -0500

    create web app

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+angular.module('webAppApp')
+  .controller('MainCtrl', function ($scope, $http) {
+    $http.get('/api/awesomeThings').success(function(awesomeThings) {
+      $scope.awesomeThings = awesomeThings;
+    });
+    $scope.year = new Date().getFullYear();
+  });
